c1f95f586a25b6a519607ec030024bfedd3aa2f6
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 17:05:32 +0530

    Update ParametersReadFromSensor.h

--- ParametersReadFromSensor.h
+++ ParametersReadFromSensor.h
@@ -1,3 +1,7 @@
 float ReadBatteryTemperature(void);
 float ReadBatteryStateOfCharge(void);
 float ReadBateryChargeRate(void);
+
+extern float TemperatureFromSensorMock;
+extern float SOCFromSensorMock;
+extern float ChargeRateFromSensorMock;
