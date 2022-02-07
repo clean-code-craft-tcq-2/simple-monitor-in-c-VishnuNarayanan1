bfd8174799924229a5cf31acf4f07cb30474306a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 15:19:18 +0530

    Create ValidateBatteryParameterLimits.c

---
+++ ValidateBatteryParameterLimits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ValidateBatteryParameterLimits.h>
+
+int ValidateIfBatteryParameterValueIsLessThanMinOperatingLimit(float minOperatingLimitOfBatteryParameter,  float batteryParameterValue, void (*Fn_Ptr)(const char[],float))
+{
+   int batteryParameterIsInSafeState = 1;
+   const char breachMessage[75] = "Battery parameter under test is less than minimum threshold limit by ";
+   if(batteryParameterValue < minOperatingLimitOfBatteryParameter)
+   {
+     batteryParameterIsInSafeState = 0;
+     ParameterBreachLevel =  (minOperatingLimitOfBatteryParameter - batteryParameterValue);
+     Fn_Ptr(breachMessage,ParameterBreachLevel);
+   }
+   return (batteryParameterIsInSafeState);
+}
+
+int ValidateIfBatteryParameterValueIsGreaterThanMaxOperatingLimit(float maxOperatingLimitOfBatteryParameter,  float batteryParameterValue, void (*Fn_Ptr)(const char[],float))
+{
+   int batteryParameterIsInSafeState = 1;
+   const char breachMessage[75] = "Battery parameter under test is greater than maximum threshold limit by ";
+   if(batteryParameterValue > maxOperatingLimitOfBatteryParameter)
+   {
+     batteryParameterIsInSafeState = 0;
+      ParameterBreachLevel = (batteryParameterValue - maxOperatingLimitOfBatteryParameter);
+      Fn_Ptr(breachMessage,ParameterBreachLevel);
+   }
+   return (batteryParameterIsInSafeState);
+}
+
