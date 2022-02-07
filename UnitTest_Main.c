b2d26db7ef4aac1d7d6919dc8eecdbce037f4db2
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 17:04:39 +0530

    Update and rename UnitTest.c to UnitTest_Main.c

--- UnitTest_Main.c
+++ UnitTest_Main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <BatteryParametersConfiguration.h>
+#include <ParametersReadFromSensor.h>
+#include <BatteryParametersUnderTest.h>
+
+#ifdef UNIT_TEST_ENVIRONMENT
 
 int Test_PrintOnConsoleWithBreachLevel = 0;
 int Test_PrintOnConsole = 0;
@@ -12,3 +17,5 @@ void PrintMessageOnConsole_Stub(char messageToBePrinted[])
 {
   Test_PrintOnConsole++;
 }
+
+#endif
