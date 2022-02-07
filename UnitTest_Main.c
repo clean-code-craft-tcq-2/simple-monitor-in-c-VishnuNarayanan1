c4983c917b48222419b964a38a8c76813f22200e
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 17:24:30 +0530

    Update UnitTest_Main.c

--- UnitTest_Main.c
+++ UnitTest_Main.c
@@ -31,11 +31,12 @@ int checkBatteryStatus(void)
   void (*Fn_Ptr_PrintMessageOnConsole)(char[]) = PrintMessageOnConsole_Stub;
   int batteryParametersUnderTest = 0;
   int OverallbatteryStatus = 0;
-  for(batteryParametersUnderTest < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
+  while(batteryParametersUnderTest < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
   {
     valueRead = batteryInputAndValidationDetails[batteryParametersUnderTest].ReadBatteryParameters();
     batteryStatus = batteryInputAndValidationDetails[batteryParametersUnderTest].ValidateBatteryParametersRead(valueRead,Fn_Ptr_PrintMessageOnConsoleWithBreachLevel,Fn_Ptr_PrintMessageOnConsole);
     OverallbatteryStatus = (OverallbatteryStatus | batteryStatus);
+    batteryParametersUnderTest++;
   }
   return OverallbatteryStatus;
 }
