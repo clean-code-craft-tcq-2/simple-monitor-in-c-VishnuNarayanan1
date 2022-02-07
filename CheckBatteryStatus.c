9695d5663a31b1301a8b9bff5a5f88969e954890
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 19:17:54 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -7,12 +7,22 @@ int CheckBatteryStatus(void (*Fn_Ptr_PrintMessageOnConsoleWithBreachLevel)(char[
   int batteryParametersUnderTest = 0;
   int OverallbatteryStatus = 0;
   int batteryStatus = 0;
+  char successMessage[75] = "Battery status is safe";
+  char failureMessage[75] = "Battery status is unsafe, call service person";
   while(batteryParametersUnderTest < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
   {
     valueRead = batteryInputAndValidationDetails[batteryParametersUnderTest].ReadBatteryParameters();
     batteryStatus = batteryInputAndValidationDetails[batteryParametersUnderTest].ValidateBatteryParametersRead(valueRead,Fn_Ptr_PrintMessageOnConsoleWithBreachLevel,Fn_Ptr_PrintMessageOnConsole);
     OverallbatteryStatus = (OverallbatteryStatus | batteryStatus);
     batteryParametersUnderTest++;
+  }
+  if(OverallbatteryStatus)
+  {
+     Fn_Ptr_PrintMessage(failureMessage);
+  }
+  else
+  {
+      Fn_Ptr_PrintMessage(successMessage);
   }
   return OverallbatteryStatus;
 }
