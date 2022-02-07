0084530e55e45f6733948ce57f94edace259435f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 15:30:57 +0530

    Update UnitTest.c

--- UnitTest.c
+++ UnitTest.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int Test_PrintOnConsoleWithBreachLevel = 0;
+int Test_PrintOnConsole = 0;
+
+void PrintMessageOnConsoleWithBreachLevel_Stub(char messageToBePrinted[], float breachLevel)
+{
+  Test_PrintOnConsoleWithBreachLevel++;
+}
+
+void PrintMessageOnConsole_Stub(char messageToBePrinted[])
+{
+  Test_PrintOnConsole++;
+}
