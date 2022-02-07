edca0f892466bbca5f271af686ee6e4d7695c7de
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 16:23:29 +0530

    Update BatteryPrametersConfiguration.h

--- BatteryPrametersConfiguration.h
+++ BatteryPrametersConfiguration.h
@@ -3,3 +3,10 @@
 #define MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE     (20)
 #define MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE     (80)
 #define MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE         (0.8)
+#define MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED          (3)
+
+struct 
+{
+  float (*Fn_PtrSensor)(void);
+  int (*Fn_PtrValidate)(float, void (*Fn_Ptr_PrintMessage)(const char[]),void (*Fn_Ptr_PrintMessageWithBreachLevel)(const char[],float));
+}batteryParam_st;
