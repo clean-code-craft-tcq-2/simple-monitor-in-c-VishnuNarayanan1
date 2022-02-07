a8bf7d993f7d0b51e5b976b0897fa8161279aa73
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 16:52:26 +0530

    Update Production_Main.c

--- Production_Main.c
+++ Production_Main.c
@@ -1,13 +1,36 @@
 #ifdef PRODUCTION_ENVIRONMENT
 #include <stdio.h>
+#include <BatteryPrametersConfiguration.h>
+#include <PrintOnConsole.h>
 
 int main()
 {
-  while(i < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
+    while(1)
+    {   
+        if(!CheckBatteryStatus())
+        {
+        }
+        else
+        {
+          exit(0);
+        }    
+    }
+}
+
+
+int checkBatteryStatus(void)
+{
+  float valueRead;
+  void Fn_Ptr_PrintMessageOnConsoleWithBreachLevel(char[], float) = PrintMessageOnConsoleWithBreachLevel;
+  void Fn_Ptr_PrintMessageOnConsole(char[]) = PrintMessageOnConsole;
+  int batteryParametersUnderTest = 0;
+  int OverallbatteryStatus = 0;
+  for(batteryParametersUnderTest < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
   {
-    
-    
-    
+    valueRead = batteryInputAndValidationDetails[batteryParametersUnderTest].ReadBatteryParameters();
+    batteryStatus = batteryInputAndValidationDetails[batteryParametersUnderTest].ValidateBatteryParametersRead(valueRead,Fn_Ptr_PrintMessageOnConsoleWithBreachLevel,Fn_Ptr_PrintMessageOnConsole);
+    OverallbatteryStatus = (OverallbatteryStatus | batteryStatus);
   }
+  return OverallbatteryStatus;
 }
 #endif
