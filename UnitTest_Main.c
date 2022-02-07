bc9e6f81d171878fa1048338e5f79ac127bd2922
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 19:18:19 +0530

    Update UnitTest_Main.c

--- UnitTest_Main.c
+++ UnitTest_Main.c
@@ -12,11 +12,13 @@ int Test_PrintOnConsole = 0;
 
 void PrintMessageOnConsoleWithBreachLevel_Stub(char messageToBePrinted[], float breachLevel)
 {
+  printf("%s %.1f",messageToBePrinted,breachLevel);
   Test_PrintOnConsoleWithBreachLevel++;
 }
 
 void PrintMessageOnConsole_Stub(char messageToBePrinted[])
 {
+  printf("\n%s\n",messageToBePrinted);
   Test_PrintOnConsole++;
 }
 
@@ -25,9 +27,10 @@ int main()
   void (*Fn_Ptr_PrintMessageOnConsoleWithBreachLevel)(char[], float) = PrintMessageOnConsoleWithBreachLevel_Stub;
   void (*Fn_Ptr_PrintMessageOnConsole)(char[]) = PrintMessageOnConsole_Stub;
   
-  TemperatureFromSensorMock = 100;
+  TemperatureFromSensorMock = 25;
   SOCFromSensorMock = 30;
-  ChargeRateFromSensorMock = 0.9;
+  ChargeRateFromSensorMock = 0.7;
+  
   CheckBatteryStatus(Fn_Ptr_PrintMessageOnConsoleWithBreachLevel,Fn_Ptr_PrintMessageOnConsole);
 }
 
