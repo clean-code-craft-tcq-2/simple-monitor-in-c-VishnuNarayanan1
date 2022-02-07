41d583c00733c1769a3d9e5c62c50a49aaaca54d
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 18:58:17 +0530

    Create CheckBatteryStatus.c

---
+++ CheckBatteryStatus.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "BatteryParametersConfiguration.h"
+
+int checkBatteryStatus(void)
+{
+  float valueRead;
+  void (*Fn_Ptr_PrintMessageOnConsoleWithBreachLevel)(char[], float) = PrintMessageOnConsoleWithBreachLevel;
+  void (*Fn_Ptr_PrintMessageOnConsole)(char[]) = PrintMessageOnConsole;
+  int batteryParametersUnderTest = 0;
+  int OverallbatteryStatus = 0;
+  int batteryStatus = 0;
+  while(batteryParametersUnderTest < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
+  {
+    valueRead = batteryInputAndValidationDetails[batteryParametersUnderTest].ReadBatteryParameters();
+    batteryStatus = batteryInputAndValidationDetails[batteryParametersUnderTest].ValidateBatteryParametersRead(valueRead,Fn_Ptr_PrintMessageOnConsoleWithBreachLevel,Fn_Ptr_PrintMessageOnConsole);
+    OverallbatteryStatus = (OverallbatteryStatus | batteryStatus);
+    batteryParametersUnderTest++;
+  }
+  return OverallbatteryStatus;
+}
