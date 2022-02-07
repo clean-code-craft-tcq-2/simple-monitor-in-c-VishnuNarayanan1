4507829aa1ca39e8d3ba1e0f009db89d9f8510d7
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-07 15:10:29 +0530

    Create PrintOnConsole.c

---
+++ PrintOnConsole.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <PrintOnConsole.h>
+
+void PrintMessageOnConsoleWithBreachLevel(char messageToBePrinted[], float breachLevel)
+{
+  printf("%s %.1f",messageToBePrinted,breachLevel);
+}
+
+void PrintMessageOnConsole(char messageToBePrinted[])
+{
+  printf("%s\n",messageToBePrinted);
+}
