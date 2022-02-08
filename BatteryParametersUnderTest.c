6ff28add29c9a8de359a60f2e8da3371047426ad
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-08 12:53:45 +0530

    Update BatteryParametersUnderTest.c

--- BatteryParametersUnderTest.c
+++ BatteryParametersUnderTest.c
@@ -3,6 +3,21 @@
 #include "BatteryParametersUnderTest.h"
 #include "ValidateBatteryParameterLimits.h"
 
+
+int CheckBatteryParameterLimits(char message[],float measuredTemperatureValueUnderTest,float minLimit, float maxLimit, 
+                                void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float), void (*Fn_Ptr_PrintMessage)(char[]))
+{
+  int batteryParameterStatus = 1;
+  Fn_Ptr_PrintMessage(message);
+  if(ValidateIfBatteryParameterValueIsLessThanMinOperatingLimit(minLimit,measuredTemperatureValueUnderTest,Fn_Ptr_PrintMessageWithBreachLevel) 
+    && ValidateIfBatteryParameterValueIsGreaterThanMaxOperatingLimit(maxLimit,measuredTemperatureValueUnderTest,Fn_Ptr_PrintMessageWithBreachLevel ) )
+  {
+    batteryParameterStatus = 0;
+  }
+  return batteryParameterStatus;
+}
+
+
 //---------------------------------------------------------------------------------------------
 /// Battery Temperature under test
 ///
