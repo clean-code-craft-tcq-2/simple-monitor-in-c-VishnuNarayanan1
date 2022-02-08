35fd099c43c119872384d07fae35a5b807ec6afe
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-08 13:10:37 +0530

    Update BatteryParametersConfiguration.c

--- BatteryParametersConfiguration.c
+++ BatteryParametersConfiguration.c
@@ -4,13 +4,26 @@
 
 struct batteryParam_st batteryInputAndValidationDetails[MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED] =
 {
-  {.ReadBatteryParameters = &ReadBatteryTemperature, 
+  {.MinimumThresholdLimit = MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
+   .MaximumThresholdLimit = MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
+    .ReadBatteryParameters = &ReadBatteryTemperature, 
    .ValidateBatteryParametersRead = &CheckBatteryTemperature
    },
-  {.ReadBatteryParameters = &ReadBatteryStateOfCharge,
+  {.MinimumThresholdLimit = MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE,
+   .MaximumThresholdLimit = MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE,
+    .ReadBatteryParameters = &ReadBatteryStateOfCharge,
    .ValidateBatteryParametersRead = &CheckBatteryStateOfCharge
    },
-  {.ReadBatteryParameters = &ReadBateryChargeRate,
+  { .MinimumThresholdLimit = MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
+    .MaximumThresholdLimit = MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE,
+    .ReadBatteryParameters = &ReadBateryChargeRate,
    .ValidateBatteryParametersRead = &CheckBatteryChargeRate
    }
 };
+
+char messageToBePrintedForBatteryParameterValidation[MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED][100] =
+{
+  "Battery parameter under test is Temperature",
+  "Battery parameter under test is State Of Charge (SOC)",
+  "Battery parameter under test is Charge rate"
+}
