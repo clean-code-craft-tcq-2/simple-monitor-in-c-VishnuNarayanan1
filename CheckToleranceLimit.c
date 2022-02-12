0970cba5bdff582e359d4d53e498c0414193e83d
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 20:26:14 +0530

    Update CheckToleranceLimit.c

--- CheckToleranceLimit.c
+++ CheckToleranceLimit.c
@@ -8,19 +8,20 @@ int CheckIfToleranceToBeChecked(int batteryParam,char message[],float measuredBa
   int functionStatus = 0;
   if(BatteryParamInfo[batteryParam].ToleranceWarningRequiredOrNot == TOLERANCE_WARNING_REQUIRED)
   {
-    functionStatus = CheckBatteryToleranceLimits(message,measuredBatteryParam, minLimit, maxLimit, toleranceLimit, Fn_Ptr_PrintMessageWithBreachLevel);
+    functionStatus = CheckBatteryToleranceLimits(message,measuredBatteryParam, minLimit, toleranceLimit, Fn_Ptr_PrintMessageWithBreachLevel);
+    functionStatus = CheckBatteryToleranceLimits(message,measuredBatteryParam, maxLimit, toleranceLimit, Fn_Ptr_PrintMessageWithBreachLevel);
   }
   return functionStatus;
 }
-int CheckBatteryToleranceLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float))
+int CheckBatteryToleranceLimits(char message[],float measuredBatteryParam, float limitToCheck, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float))
 {
   float toleranceValue;
   int functionStatus = 1;
   char toleranceMessageMin[75] = "Battery parameter under test is less than minimum tolerance limit by ";
   char toleranceMessageMax[75] = "Battery parameter under test is greater than maximum tolerance limit by ";
   toleranceValue = ReturnToleranceLimit(toleranceLimit,maxLimit);
-    if(ValidateBatteryParamMinLimit((minLimit + toleranceValue), measuredBatteryParam, toleranceMessageMin, Fn_Ptr_PrintMessageWithBreachLevel) 
-    && ValidateBatteryParamMaxLimit((maxLimit - toleranceValue), measuredBatteryParam, toleranceMessageMax, Fn_Ptr_PrintMessageWithBreachLevel))
+    if(ValidateBatteryParamMinLimit((limitToCheck + toleranceValue), measuredBatteryParam, toleranceMessageMin, Fn_Ptr_PrintMessageWithBreachLevel) 
+    && ValidateBatteryParamMaxLimit(limitToCheck, measuredBatteryParam, toleranceMessageMax, Fn_Ptr_PrintMessageWithBreachLevel))
   {
       functionStatus = 0;
   }
