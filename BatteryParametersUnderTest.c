4596c96629f71041f1eb9d3dc469352ff58b3938
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 16:58:51 +0530

    Update BatteryParametersUnderTest.c

--- BatteryParametersUnderTest.c
+++ BatteryParametersUnderTest.c
@@ -3,15 +3,14 @@
 #include "BatteryParametersUnderTest.h"
 #include "ValidateBatteryParameterLimits.h"
 
-
 int CheckBatteryParameterLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float))
 {
   int batteryParameterStatus = 1;
   char breachMessageMin[75] = "Battery parameter under test is less than minimum threshold limit by ";
   char breachMessageMax[75] = "Battery parameter under test is greater than maximum threshold limit by ";
   Fn_Ptr_PrintMessageWithBreachLevel(message,BREACH_LEVEL_NEED_NOT_BE_PRINTED);
-  if(ValidateBatteryParamMinLimit(minLimit,measuredBatteryParam,breachMessageMin,Fn_Ptr_PrintMessageWithBreachLevel) 
-    && ValidateBatteryParamMaxLimit(maxLimit,measuredBatteryParam,breachMessageMax,Fn_Ptr_PrintMessageWithBreachLevel))
+  if(ValidateBatteryParamMinLimit(minLimit, measuredBatteryParam, breachMessageMin, Fn_Ptr_PrintMessageWithBreachLevel) 
+    && ValidateBatteryParamMaxLimit(maxLimit, measuredBatteryParam, breachMessageMax, Fn_Ptr_PrintMessageWithBreachLevel))
   {
     batteryParameterStatus = 0;
   }
