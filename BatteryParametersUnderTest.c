15ac4dbfbb134e111daa551b8b77cf9b7b9de9ab
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 21:40:33 +0530

    Update BatteryParametersUnderTest.c

--- BatteryParametersUnderTest.c
+++ BatteryParametersUnderTest.c
@@ -8,7 +8,7 @@ int CheckBatteryParameterLimits(char message[],float measuredBatteryParam,float 
   int batteryParameterStatus = 1;
   Fn_Ptr_PrintMessageWithBreachLevel(message,BREACH_LEVEL_NEED_NOT_BE_PRINTED);
   if(ValidateBatteryParamMinLimit(minLimit, measuredBatteryParam, BreachMessage[LanguageChoosen], Fn_Ptr_PrintMessageWithBreachLevel) 
-    && ValidateBatteryParamMaxLimit(maxLimit, measuredBatteryParam, BreachMessage[[LanguageChoosen + 2], Fn_Ptr_PrintMessageWithBreachLevel))
+    && ValidateBatteryParamMaxLimit(maxLimit, measuredBatteryParam, BreachMessage[LanguageChoosen + 2], Fn_Ptr_PrintMessageWithBreachLevel))
   {
     batteryParameterStatus = 0;
   }
