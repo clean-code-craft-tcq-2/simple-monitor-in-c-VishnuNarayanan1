4860cc59c20f66d018fdc6e4dda047bd905c7a2a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 16:47:31 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -6,9 +6,7 @@
 int CheckBatteryStatus(void (*Fn_Ptr_PrintMessageOnConsoleWithBreachLevel)(char[], float))
 {
   float valueRead;
-  int batteryParametersUnderTest = 0;
-  int OverallbatteryStatus = 0;
-  int batteryStatus = 0;
+  int batteryParametersUnderTest = 0, OverallbatteryStatus = 0, batteryStatus = 0;
   char successMessage[75] = "Battery status is safe !!!";
   char failureMessage[75] = "Battery status is unsafe, call service person !!!";
   char messageToBePrintedOnConsole[100];
