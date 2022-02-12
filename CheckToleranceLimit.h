6e566613bd52140dd34db55f156fd5400fb7864e
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 21:29:12 +0530

    Delete CheckToleranceLimit.h

--- CheckToleranceLimit.h
+++
@@ -1,3 +0,0 @@
-int CheckIfToleranceToBeChecked(int batteryParam,char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float));
-int CheckBatteryToleranceLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float));
-float ReturnToleranceLimit(float tolerancePercentage,  float batteryParameterValue);
