bac697c5a8b1f2008d2d7abeb02ac9e8b0343338
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 19:26:59 +0530

    Update CheckToleranceLimit.c

--- CheckToleranceLimit.c
+++ CheckToleranceLimit.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "CheckToleranceLimit.h"
+#include "BatteryParametersConfiguration.h"
 #include "ValidateBatteryParameterLimits.h"
 
-int CheckBatteryToleranceLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float))
+int CheckBatteryToleranceLimits(int batteryParam,char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float))
 {
   float toleranceValue;
   char toleranceMessageMin[75] = "Battery parameter under test is less than minimum tolerance limit by ";
   char toleranceMessageMax[75] = "Battery parameter under test is greater than maximum tolerance limit by ";
   toleranceValue = ReturnToleranceLimit(toleranceLimit,maxLimit);
-    if(ValidateBatteryParamMinLimit((minLimit + toleranceValue), measuredBatteryParam, toleranceMessageMin, Fn_Ptr_PrintMessageWithBreachLevel) 
+    if((BatteryParamInfo[batteryParam].ToleranceWarningRequiredOrNot == TOLERANCE_WARNING_REQUIRED)
+      && ValidateBatteryParamMinLimit((minLimit + toleranceValue), measuredBatteryParam, toleranceMessageMin, Fn_Ptr_PrintMessageWithBreachLevel) 
     && ValidateBatteryParamMaxLimit((maxLimit - toleranceValue), measuredBatteryParam, toleranceMessageMax, Fn_Ptr_PrintMessageWithBreachLevel))
   {
   }
