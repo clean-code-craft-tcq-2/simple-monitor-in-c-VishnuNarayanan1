d8224de3509224d85f6b3c1b87eead39a06a6903
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 12:17:42 +0530

    Update BatteryParametersConfiguration.h

--- BatteryParametersConfiguration.h
+++ BatteryParametersConfiguration.h
@@ -1,12 +1,17 @@
 #define UNIT_TEST_ENVIRONMENT
-#define MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE         (0)
-#define MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE         (45)
-#define MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE     (20)
-#define MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE     (80)
-#define MIN_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE         (0.2)
-#define MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE         (0.8)
-#define MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED          (3)
-#define BREACH_LEVEL_NEED_NOT_BE_PRINTED                (0xffffff)
+#define MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE           (0)
+#define MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE           (45)
+#define TOLERANCE_PERCENTAGE_LIMIT_BATTERY_TEMPERATURE    (5)
+#define MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE       (20)
+#define MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE       (80)
+#define TOLERANCE_PERCENTAGE_LIMIT_BATTERY_STATE_OF_CHARGE(5)
+#define MIN_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE           (0.2)
+#define MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE           (0.8)
+#define MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED            (3)
+#define TOLERANCE_PERCENTAGE_LIMIT_BATTERY_CHARGE_RATE    (0xffffff)
+#define BREACH_LEVEL_NEED_NOT_BE_PRINTED                  (0xffffff)
+#define TOLERANCE_WARNING_REQUIRED                        (1)
+#define TOLERANCE_WARNING_NOT_REQUIRED                    (0)
 
 typedef float (*Fn_Ptr_ReadBatteryParameters)(void);
 
@@ -14,6 +19,8 @@ struct batteryParam_st
 {
   float MinimumThresholdLimit;
   float MaximumThresholdLimit;
+  float ToleranceLimit;
+  bool ToleranceWarningRequiredOrNot;
   Fn_Ptr_ReadBatteryParameters ReadBatteryParameters;  
 };
 
