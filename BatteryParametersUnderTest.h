bcb96ddc2a8eb4a39afce040f9907ad454dd1699
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 16:54:55 +0530

    Update BatteryParametersUnderTest.h

--- BatteryParametersUnderTest.h
+++ BatteryParametersUnderTest.h
@@ -1,2 +1,1 @@
-int CheckBatteryParameterLimits(char message[],float measuredTemperatureValueUnderTest,float minLimit, float maxLimit, 
-                                void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float));
+int CheckBatteryParameterLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float));
