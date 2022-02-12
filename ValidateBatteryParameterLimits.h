b9743a602ff9bcef0f25b44b97651c547a7557ad
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 14:52:45 +0530

    Update ValidateBatteryParameterLimits.h

--- ValidateBatteryParameterLimits.h
+++ ValidateBatteryParameterLimits.h
@@ -1,2 +1,2 @@
-int ValidateBatteryParamMinLimit(float minLimitOfBatteryParameter,  float batteryParameterValue, void (*Fn_Ptr)(char[],float));
-int ValidateBatteryParamMaxLimit(float maxLimitOfBatteryParameter,  float batteryParameterValue, void (*Fn_Ptr)(char[],float));
+int ValidateBatteryParamMinLimit(float minLimitOfBatteryParameter,  float batteryParameterValue, char messageToPrint[], void (*Fn_Ptr)(char[],float));
+int ValidateBatteryParamMaxLimit(float maxLimitOfBatteryParameter,  float batteryParameterValue,char messageToPrint[], void (*Fn_Ptr)(char[],float));
