91b9e6f18eec2d631aa7f7006ebecd2cdd391f3a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 12:17:37 +0530

    Update BatteryParametersConfiguration.c

--- BatteryParametersConfiguration.c
+++ BatteryParametersConfiguration.c
@@ -6,14 +6,20 @@ struct batteryParam_st batteryInputAndValidationDetails[MAX_BATTERY_PARAMETERS_T
 {
   {.MinimumThresholdLimit = MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
    .MaximumThresholdLimit = MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
+   .ToleranceLimit = TOLERANCE_PERCENTAGE_LIMIT_BATTERY_TEMPERATURE,
+    .ToleranceWarningRequiredOrNot = TOLERANCE_WARNING_REQUIRED,
     .ReadBatteryParameters = &ReadBatteryTemperature, 
    },
   {.MinimumThresholdLimit = MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE,
    .MaximumThresholdLimit = MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE,
+   .ToleranceLimit = TOLERANCE_PERCENTAGE_LIMIT_BATTERY_STATE_OF_CHARGE,
+   .ToleranceWarningRequiredOrNot = TOLERANCE_WARNING_REQUIRED,
    .ReadBatteryParameters = &ReadBatteryStateOfCharge,
    },
   { .MinimumThresholdLimit = MIN_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE,
     .MaximumThresholdLimit = MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE,
+   .ToleranceLimit = TOLERANCE_PERCENTAGE_LIMIT_BATTERY_CHARGE_RATE,
+   .ToleranceWarningRequiredOrNot = TOLERANCE_WARNING_NOT_REQUIRED,
     .ReadBatteryParameters = &ReadBateryChargeRate,
    }
 };
