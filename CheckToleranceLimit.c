45991acb57ef5f06a3ad384d508734c6fa78e9cf
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 19:04:33 +0530

    Update CheckToleranceLimit.c

--- CheckToleranceLimit.c
+++ CheckToleranceLimit.c
@@ -5,8 +5,8 @@
 int CheckBatteryToleranceLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float))
 {
   float toleranceValue;
-  char breachMessageMin[75] = "Battery parameter under test is less than minimum tolerance limit by ";
-  char breachMessageMax[75] = "Battery parameter under test is greater than maximum tolerance limit by ";
+  char toleranceMessageMin[75] = "Battery parameter under test is less than minimum tolerance limit by ";
+  char toleranceMessageMax[75] = "Battery parameter under test is greater than maximum tolerance limit by ";
   toleranceValue = ReturnToleranceLimit(toleranceLimit,maxLimit);
     if(ValidateBatteryParamMinLimit((minLimit + toleranceValue), measuredBatteryParam, toleranceMessageMin, Fn_Ptr_PrintMessageWithBreachLevel) 
     && ValidateBatteryParamMaxLimit((maxLimit - toleranceValue), measuredBatteryParam, toleranceMessageMax, Fn_Ptr_PrintMessageWithBreachLevel))
