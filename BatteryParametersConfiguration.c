7722a0a30426b46288c7883bf0fd4e0f8e7a3b44
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 21:38:32 +0530

    Update BatteryParametersConfiguration.c

--- BatteryParametersConfiguration.c
+++ BatteryParametersConfiguration.c
@@ -29,4 +29,9 @@ char MessageToBePrinted[MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED * MAX_POSSIBLE_LA
 "Battery parameter under test is State Of Charge (SOC)", "Der zu testende Batterieparameter ist der Ladezustand (SOC)",
 "Battery parameter under test is Charge rate", "Der zu testende Batterieparameter ist die Laderate"};
 
+
+char BreachMessage[2 * MAX_POSSIBLE_LANGUAGES][100] =
+{"Battery parameter under test is less than minimum threshold limit by ","Der zu testende Batterieparameter ist kleiner als der minimale Schwellenwert um",
+ {"Battery parameter under test is greater than maximum threshold limit by ","Der zu testende Batterieparameter ist größer als der maximale Schwellenwert um"};
+
 enum languages LanguageChoosen = German;
