e0ad37282867fe047ebdf22ba79ee44ec0863a4a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 19:02:17 +0530

    Update CheckToleranceLimit.h

--- CheckToleranceLimit.h
+++ CheckToleranceLimit.h
@@ -1,1 +1,2 @@
+int CheckBatteryToleranceLimits(char message[],float measuredBatteryParam,float minLimit, float maxLimit, float toleranceLimit, void (*Fn_Ptr_PrintMessageWithBreachLevel)(char[],float));
 float ReturnToleranceLimit(float tolerancePercentage,  float batteryParameterValue);
