dced7d121eb4a48b27d9581757c6477555f718f3
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-12 19:25:52 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -12,11 +12,9 @@ int CheckBatteryStatus(void (*FnPtrPrintMsgOnConsoleWithBreachLevel)(char[], flo
   while(batteryParameter < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
   {
     valueRead = BatteryParamInfo[batteryParameter].ReadBatteryParameters();
-    if(BatteryParamInfo[batteryParameter].ToleranceWarningRequiredOrNot == TOLERANCE_WARNING_REQUIRED)
-    {
-      CheckBatteryToleranceLimits(MessageToBePrinted[batteryParameter], valueRead, BatteryParamInfo[batteryParameter].MinimumThreshold,
+   CheckBatteryToleranceLimits(batteryParameter,MessageToBePrinted[batteryParameter], valueRead, BatteryParamInfo[batteryParameter].MinimumThreshold,
                                    BatteryParamInfo[batteryParameter].MaximumThreshold,BatteryParamInfo[batteryParameter].ToleranceLimit ,FnPtrPrintMsgOnConsoleWithBreachLevel);
-    }
+    
     batteryStatus = CheckBatteryParameterLimits(MessageToBePrinted[batteryParameter], valueRead, BatteryParamInfo[batteryParameter].MinimumThreshold,
                                                 BatteryParamInfo[batteryParameter].MaximumThreshold,FnPtrPrintMsgOnConsoleWithBreachLevel);
     OverallbatteryStatus = (OverallbatteryStatus | batteryStatus);
