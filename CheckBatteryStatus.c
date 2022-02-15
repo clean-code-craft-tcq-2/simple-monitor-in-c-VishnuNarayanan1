7b68cabc7009d308188a1567ca1bf68e55135189
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-15 22:39:11 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -8,7 +8,7 @@ int CheckBatteryStatus(void (*FnPtrPrintMsgOnConsoleWithBreachLevel)(char[], flo
   int batteryParameter = 0, OverallbatteryStatus = 0, batteryStatus = 0;
   while(batteryParameter < MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED)
   {
-    paramValues = ReadBatteryParameters[batteryParameter].FnPtrReadBatteryParam ();
+    paramValues = ReadBatteryParameters[batteryParameter].readInputValues();
     batteryStatus = CheckBatteryParameterLimits(MessageToBePrinted[(batteryParameter * MAX_POSSIBLE_LANGUAGES) + LanguageChoosen], paramValues.Value, paramValues.MinLimit,
                                                 paramValues.MaxLimit,FnPtrPrintMsgOnConsoleWithBreachLevel);
     OverallbatteryStatus = (OverallbatteryStatus | batteryStatus);
