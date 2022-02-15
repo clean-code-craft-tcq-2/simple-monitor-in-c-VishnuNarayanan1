02151e4510896eec478c8ee17b1d9c059912cd7d
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-15 19:58:58 +0530

    Update BatteryParametersConfiguration.h

--- BatteryParametersConfiguration.h
+++ BatteryParametersConfiguration.h
@@ -3,17 +3,12 @@
 #define UNIT_TEST_ENVIRONMENT
 #define MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE           (0)
 #define MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE           (45)
-#define TOLERANCE_PERCENTAGE_LIMIT_BATTERY_TEMPERATURE    (5)
 #define MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE       (20)
 #define MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE       (80)
-#define TOLERANCE_PERCENTAGE_LIMIT_BATTERY_STATE_OF_CHARGE (5)
 #define MIN_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE           (0.2)
 #define MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE           (0.8)
 #define MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED            (3)
-#define TOLERANCE_PERCENTAGE_LIMIT_BATTERY_CHARGE_RATE    (0xffffff)
 #define BREACH_LEVEL_NEED_NOT_BE_PRINTED                  (0xffffff)
-#define TOLERANCE_WARNING_REQUIRED                        (true)
-#define TOLERANCE_WARNING_NOT_REQUIRED                    (false)
 
 typedef float (*FnPtrReadBatteryParam)(void);
 
@@ -21,8 +16,6 @@ struct BatteryParam_st
 {
   float MinimumThreshold;
   float MaximumThreshold;
-  float ToleranceLimit;
-  bool ToleranceWarningRequiredOrNot;
   FnPtrReadBatteryParam ReadBatteryParameters;  
 };
 
