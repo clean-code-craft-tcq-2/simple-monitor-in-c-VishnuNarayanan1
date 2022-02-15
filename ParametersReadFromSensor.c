d365e65f2fdcbd914f4c6f4131ee6c855eebf6ef
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-15 20:07:22 +0530

    Update ParametersReadFromSensor.c

--- ParametersReadFromSensor.c
+++ ParametersReadFromSensor.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
 #include "ParametersReadFromSensor.h"
+#include "BatteryParametersConfiguration.h"
 
 float TemperatureFromSensorMock;
 float SOCFromSensorMock;
 float ChargeRateFromSensorMock;
 
-float ReadBatteryTemperature(void)
+ValueLimits_st ReadBatteryTemperature(void)
 {
-  return TemperatureFromSensorMock;
+  struct ValueLimits_st tempLimits;
+  if(tempUnit == Farenheit)
+  {
+    tempLimits.MinLimit = ConvertCelciusToFarenheit(MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE);
+    tempLimits.MaxLimit = ConvertCelciusToFarenheit(MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE);
+    tempLimits.Value = ConvertCelciusToFarenheit(TemperatureFromSensorMock);
+  }
+  else
+  {
+    tempLimits.MinLimit = MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE;
+    tempLimits.MaxLimit = MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE;
+    tempLimits.Value = TemperatureFromSensorMock;
+  }
+  return tempLimits;
 }
 
-float ReadBatteryStateOfCharge(void)
+ValueLimits_st ReadBatteryStateOfCharge(void)
 {
-  return SOCFromSensorMock;
+  struct ValueLimits_st stateOfChargeLimits;
+  stateOfChargeLimits.MinLimit = MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE;
+  stateOfChargeLimits.MaxLimit = MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE;
+  stateOfChargeLimits.Value = SOCFromSensorMock;
+  return stateOfChargeLimits;
 }
 
-float ReadBateryChargeRate(void)
+ValueLimits_st ReadBateryChargeRate(void)
 {
-  return ChargeRateFromSensorMock;
+  struct ValueLimits_st chargeRateLimits;
+  chargeRateLimits.MinLimit = MIN_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE;
+  chargeRateLimits.MaxLimit = MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE ;
+  chargeRateLimits.Value = ChargeRateFromSensorMock;
+  return chargeRateLimits;
+}
+  
+float ConvertCelciusToFarenheit(float farenheit)
+{
+    return ((farenheit - 32) * 5 / 9);
 }
