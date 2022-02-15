76e09f9d0f30c56db04f3e45f21c03d70b1bd6fe
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-15 20:16:39 +0530

    Update ParametersReadFromSensor.h

--- ParametersReadFromSensor.h
+++ ParametersReadFromSensor.h
@@ -1,4 +1,4 @@
 ValueLimits_st ReadBatteryTemperature(void);
-ValueLimits_st ReadBatteryStateOfCharge(void)'
+ValueLimits_st ReadBatteryStateOfCharge(void);
 ValueLimits_st ReadBateryChargeRate(void);
 float ConvertCelciusToFarenheit(float farenheit);
