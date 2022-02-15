41974578438dafb26c2f09ca88bc63dfadc01807
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-15 20:13:32 +0530

    Update BatteryParametersConfiguration.c

--- BatteryParametersConfiguration.c
+++ BatteryParametersConfiguration.c
@@ -2,27 +2,15 @@
 #include "ParametersReadFromSensor.h"
 #include "BatteryParametersUnderTest.h"
 
-struct BatteryParam_st BatteryParamInfo[MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED] =
+FnPtrReadBatteryParam ReadBatteryParameters[MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED] =
 {
-  {.MinimumThreshold = MIN_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
-   .MaximumThreshold = MAX_THRESHOLD_LIMIT_BATTERY_TEMPERATURE,
-    .ReadBatteryParameters = &ReadBatteryTemperature, 
-   },
-  {.MinimumThreshold = MIN_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE,
-   .MaximumThreshold = MAX_THRESHOLD_LIMIT_BATTERY_STATE_OF_CHARGE,
-   .ReadBatteryParameters = &ReadBatteryStateOfCharge,
-   },
-  { .MinimumThreshold = MIN_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE,
-    .MaximumThreshold = MAX_THRESHOLD_LIMIT_BATTERY_CHARGE_RATE,
-    .ReadBatteryParameters = &ReadBateryChargeRate,
-   }
-};
+  &ReadBatteryTemperature,&ReadBatteryStateOfCharge,&ReadBateryChargeRate
+}
 
 char MessageToBePrinted[MAX_BATTERY_PARAMETERS_TO_BE_VALIDATED * MAX_POSSIBLE_LANGUAGES][100] =
 {"Battery parameter under test is Temperature", "Der zu testende Batterieparameter ist Temperatur",
 "Battery parameter under test is State Of Charge (SOC)", "Der zu testende Batterieparameter ist der Ladezustand (SOC)",
 "Battery parameter under test is Charge rate", "Der zu testende Batterieparameter ist die Laderate"};
-
 
 char BreachMessage[2 * MAX_POSSIBLE_LANGUAGES][100] =
 {"Battery parameter under test is less than minimum threshold limit by ","Der zu testende Batterieparameter ist kleiner als der minimale Schwellenwert um",
